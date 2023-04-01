6f864c8b112cf0e776144f4581a2796878c8e7a6
Arpit078 <verma.arpit078@.com>
2023-04-01 14:00:52 +0530

    trial package

--- post_cron.js
+++ post_cron.js
@@ -1,15 +1,21 @@
 import postCreate from "./post_create/action.js"
 import cron from 'node-cron';
 import express from "express"
+import axios from "axios"
 // Run program at 3pm and 8pm
-cron.schedule('0 12,17 * * *', () => {
+
+cron.schedule('5 14 * * *', () => {
     postCreate()
 });
+
+// cron.schedule('0 12,17 * * *', () => {
+//     postCreate()
+// });
 
 
 
 const url = `https://post-create.onrender.com/`
-cron.schedule('0 */14 * * * *', () => {
+cron.schedule('0 */1 * * * *', () => {
 
 axios.get(url, { 
     headers: { "Accept-Encoding": "gzip,deflate,compress" } 
